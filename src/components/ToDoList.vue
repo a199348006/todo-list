571c61fabcd0d5d054ad1d2f43dac29c192280de
sh.chen <sh.chen@shinkong.com.tw>
2021-12-15 16:39:23 +0800

    css: sort out todo-list & done-list

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -6,31 +6,37 @@
       </template>
     </el-input>
 
-    {{ input }}
-
     <el-card class="todo-list">
       <template #header>
         <span>未完成</span>
-        <el-badge :value="todoItem.length" class="item"></el-badge>
+        <el-badge :value="todoItem.length"></el-badge>
       </template>
-      <div v-for="(todo, index) in todoItem" :key="index">
-        {{ index }} {{ todo }}
-        <el-button type="success" size="mini" circle @click="completeItem(index, todo)"><el-icon :size="20"><Check /></el-icon></el-button>         
-        <el-button type="danger" size="mini" circle @click="removeItem(index)"><el-icon :size="20"><Minus /></el-icon></el-button>    
+      <div v-for="(todo, index) in todoItem" :key="index" class="item">
+        <el-row>
+          <el-col :span="20" class="item-title">{{ todo }}</el-col>
+          <el-col :span="4" class="item-button">
+            <el-button type="success" size="mini" circle @click="completeItem(index, todo)"><el-icon :size="20"><Check /></el-icon></el-button>         
+            <el-button type="danger" size="mini" circle @click="removeItem(index)"><el-icon :size="20"><Minus /></el-icon></el-button> 
+          </el-col>
+        </el-row>
       </div>
-    </el-card>
+    </el-card>    
 
     <el-card class="todo-list">
       <template #header>
         <span>完成</span>
-        <el-badge :value="doneItem.length" class="item"></el-badge>
+        <el-badge :value="doneItem.length"></el-badge>
       </template>
-      <div v-for="(done, index) in doneItem" :key="index">
-        {{ index }} {{ done }}
-        <el-button type="primary" size="mini" circle @click="back2todo(index, done)"><el-icon :size="20"><RefreshLeft /></el-icon></el-button> 
+      <div v-for="(done, index) in doneItem" :key="index" class="item done" style="border-color: #708090;">
+        <el-row>
+          <el-col :span="20" class="item-title">{{ done }}</el-col>
+          <el-col :span="4" class="item-button">
+            <el-button type="primary" size="mini" circle @click="back2todo(index, done)"><el-icon :size="20"><RefreshLeft /></el-icon></el-button> 
+          </el-col>
+        </el-row>
       </div>
-    </el-card>    
-      
+    </el-card>
+ 
   </div>
 </template>
 
@@ -80,5 +86,30 @@ export default {
 
 .todo-list {
   margin: 10px 0px;
+}
+
+.item {
+  border-width: 1px;
+  border-color: #FF8C00;
+  border-style: solid;
+  padding: 3px;
+  margin: 2px 0px;
+}
+
+.item-title {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  word-break: break-all;
+}
+
+.item-button {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.done {
+  background-color: #CDCDCD;
 }
 </style>
